23eb9996c07127edb508dfc1415b142d34da3c18
rottwelliaz <francisco.flores2101@alumnos.ubiobio.cl>
2025-11-18 14:54:20 -0300

    uniendo requisitos

--- backend/src/routes/asistenciaEv.routes.js
+++ backend/src/routes/asistenciaEv.routes.js
@@ -2,26 +2,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { checkRole } from "../middleware/role.middleware.js";
-import { marcarAsistencia,
-        asignarNota,
-        getNotasPorEstudiante,
-        editarNota,
-        eliminarNota,
-        getTodasNotas } from "../controllers/asistenciaEv.controller.js";
+import { marcarAsistencia, asignarNota } from "../controllers/asistenciaEv.controller.js";
 
 const router = Router();
 
 
 router.post("/marcarAsistencia", authMiddleware, marcarAsistencia);
-
-router.get("/", authMiddleware, checkRole("profesor", "admin"), getTodasNotas);
-
-router.get("/student/:studentId", authMiddleware, getNotasPorEstudiante);
-
 router.put("/:id/nota", authMiddleware, checkRole("profesor"), asignarNota);
-
-router.put("/:id", authMiddleware, checkRole("profesor", "admin"), editarNota);
-
-router.delete("/:id", authMiddleware, checkRole("profesor", "admin"), eliminarNota);
 
 export default router;
